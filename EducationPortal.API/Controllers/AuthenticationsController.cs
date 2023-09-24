24ba6c4c26bb27e237723b977cfa9260289cbe0c
sinemhizz <sinemhiz66@gmail.com>
2023-09-24 12:39:06 +0300

    Add(Authentication): AuthenticationController is added.

---
+++ EducationPortal.API/Controllers/AuthenticationsController.cs
@@ -0,0 +1,18 @@
+﻿using EducationPortal.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPortal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationsController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationsController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+    }
+}
